e14bd847923862e3695dea4491c6f73e6fb995d9
Daiki Saito <dsflon@gmail.com>
2025-05-20 15:53:54 +0900

    chroe

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@modelcontextprotocol/sdk'],
-  
-  experimental: {
-    esmExternals: 'loose',
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
